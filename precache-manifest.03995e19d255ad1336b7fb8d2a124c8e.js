d8cbef4632e8d25f7037ec0b2dfec53449cf79fc
Dorin Popa <dorin.popa1995@gmail.com>
2019-10-10 10:08:58 +0300

    Updates

---
+++ precache-manifest.03995e19d255ad1336b7fb8d2a124c8e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0098c06f8ee19d3e5fbd600363cd516f",
+    "url": "/retently-task/index.html"
+  },
+  {
+    "revision": "13cc3346365042f54bd1",
+    "url": "/retently-task/static/css/main.5dd45386.chunk.css"
+  },
+  {
+    "revision": "df7205a9e74a1cc62f9b",
+    "url": "/retently-task/static/js/2.3940c153.chunk.js"
+  },
+  {
+    "revision": "13cc3346365042f54bd1",
+    "url": "/retently-task/static/js/main.9fe3c90b.chunk.js"
+  },
+  {
+    "revision": "1dfe29240e818dba576a",
+    "url": "/retently-task/static/js/runtime-main.00eacd91.js"
+  },
+  {
+    "revision": "14a92790b6d688db080c8794d2eecd2a",
+    "url": "/retently-task/static/media/Heebo-ExtraBold.14a92790.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/retently-task/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "a05f1fb32996567bf53cc6d415cc5e55",
+    "url": "/retently-task/static/media/bg.a05f1fb3.png"
+  },
+  {
+    "revision": "b548d919eaf6d6925afb830a693bb0b6",
+    "url": "/retently-task/static/media/check.b548d919.svg"
+  },
+  {
+    "revision": "63e85282245c3703d949a78709326875",
+    "url": "/retently-task/static/media/cloud_upload.63e85282.svg"
+  },
+  {
+    "revision": "9f2b38f6036d5fa816e9b5c4fad1a905",
+    "url": "/retently-task/static/media/css.9f2b38f6.svg"
+  },
+  {
+    "revision": "c43ecc80880ea97075f0e548b554e005",
+    "url": "/retently-task/static/media/html.c43ecc80.svg"
+  },
+  {
+    "revision": "6b247234a6530e66bf12deb5b9e4bfdb",
+    "url": "/retently-task/static/media/right_image.6b247234.png"
+  },
+  {
+    "revision": "c4d15ed4f02ca74afdb836b4ec3e73ce",
+    "url": "/retently-task/static/media/right_image@2x.c4d15ed4.png"
+  }
+]);
